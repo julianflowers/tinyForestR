dbc0b1f5b4546f4d3344220875ee56eed7f2abb6
julianflowers <julianflowers@users.noreply.github.com>
2023-05-15 20:27:51 +0100

    update

--- R/convert_lat_lon_BNG.R
+++ R/convert_lat_lon_BNG.R
@@ -29,8 +29,8 @@
 os_lat_lon_to_grid <- function(lon, lat){
 
   require(reticulate); require(tidyverse)
-  virtualenv_remove("tinyforest")
-  virtualenv_create("tinyforest", system_site_packages = TRUE)
+  #virtualenv_remove("tinyforest")
+  #virtualenv_create("tinyforest", system_site_packages = TRUE)
   Sys.setenv(RETICULATE_PYTHON = "/Users/julianflowers/.virtualenvs/tinyforest/bin/python")
   use_virtualenv("tinyforest")
 
