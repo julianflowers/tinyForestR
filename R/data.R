c11ab88b395980b98cc1039fdb873f26c272a4b2
julianflowers <julianflowers@users.noreply.github.com>
2023-05-26 07:52:32 +0100

    updates

--- R/data.R
+++ R/data.R
@@ -16,4 +16,42 @@
 
 #'   ...
 #' }
+#'
+#'
+#' #' NBN Atlas data for 1km buffer round each Tiny Forest location 2015-2023: flowering plants
+#'
+#' A subset of data from the NBN Atlas dataset
+#'
+#'
+#' @format ## `flowers_filt`
+#' A data frame with 23,143 rows and 15 columns:
+#' \describe{
+#'   \item{kingdom}{Animal kingdowm}
+#'   \item{phylum, classs, order, family, genus}{taxomomic classification}
+#'   \item{decimalLatiturde, decimalLongitude}{decimal coordinates}
+#'   \item{year, month}{Year and month of collection}
+#'   \item{dataProviderName}{data source}
+#'   \item{speciesGrouns, vernacularName, species}{species common and scientific name}
+#'   \item{tf_id}{tinyforest id}
 
+#'   ...
+#' }
+
+#' NBN Atlas data for 1km buffer round each Tiny Forest location 2015-2023: insects
+#'
+#' A subset of data from the NBN Atlas dataset
+#'
+#'
+#' @format ## `insects_filt`
+#' A data frame with 45,597 rows and 15 columns:
+#' \describe{
+#'   \item{kingdom}{Animal kingdowm}
+#'   \item{phylum, classs, order, family, genus}{taxomomic classification}
+#'   \item{decimalLatiturde, decimalLongitude}{decimal coordinates}
+#'   \item{year, month}{Year and month of collection}
+#'   \item{dataProviderName}{data source}
+#'   \item{speciesGrouns, vernacularName, species}{species common and scientific name}
+#'   \item{tf_id}{tinyforest id}
+
+#'   ...
+#' }
